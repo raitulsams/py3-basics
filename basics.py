1eb4574cda636ac63218fd0bd3dd2a29d2f508ab
raitulsams <raitulsams202@gmail.com>
2025-01-11 14:26:11 +0100

    data types

---
+++ basics.py
@@ -0,0 +1,53 @@
+print("Jibon ta bedona")
+
+
+from operator import truediv
+
+
+def strings_():
+    c = "Hello, World"
+    d = c.split(",")
+    print(c.split(","))
+    print(type(d))
+
+    txt = "I am \"watching\" vikings"
+
+    print(txt)
+
+    txt2 = "Banana is not my favorite"
+    txt3 = txt2.center(200, "3")
+    print(txt3)
+
+    x = 2go
+    y = 5
+
+    print(x ** y)  # same as 2*2*2*2*2
+
+def variables():
+    x, y, z = "Orange", "Mango", "Sams"
+    print(x+" "+y+" "+z)
+    print(x+y+z)
+
+def casting():
+    x = int(1.2)
+    y= float(12.4)
+    z = str("1")
+    print(x)
+    print(y)
+    print(z)
+
+def boolean():
+    z = True
+    y = False
+
+    t = 100
+    b = 400
+
+    if t > b:
+        print("{t} is greater than {b}".format(t=t, b=b))
+    if t < b:
+        print("{t} is less than {b}".format(t=t, b=b))
+    print(z)
+    print(y)
+
+boolean()
--- py-basics.py
+++
@@ -1,1 +0,0 @@
-print("Jibon ta bedona")
