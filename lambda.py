1daba4c25d4dd4427881d4139713f3930415648a
raitulsams <raitulsams202@gmail.com>
2025-01-26 22:14:49 +0100

    sets principle

--- lambda.py
+++ lambda.py
@@ -14,4 +14,43 @@ def myLambdaFunc(a, b):
 
 
 myDouble = myLambdaFunc(2, 3)
-print(myDouble(4))
+
+
+# print(myDouble(4))
+
+def mycustomsort():
+    def myval(item):
+        return item['year']
+
+    carModel = ["BMW", "AORD", "MITSUBISHI", "VW"]
+    cars = [
+        {'car': 'Ford', 'year': 2005},
+        {'car': 'Mitsubishi', 'year': 2000},
+        {'car': 'BMW', 'year': 2019},
+        {'car': 'VW', 'year': 2011}
+    ]
+    cars.sort(key=myval)
+    carModel.sort()
+    print(carModel)
+    print(cars)
+
+
+# mycustomsort()
+
+
+def lambSort():
+    # Sort a list of tuples by the second element
+    data = [(10, 'b'), (2, 'a'), (3, 'c')]
+    data.sort(key=lambda x: x[1])
+    print(data)  # Output: [(2, 'a'), (1, 'b'), (3, 'c')]
+
+
+# lambSort()
+
+
+def filter_lamb():
+    x = [1, 23, 4, 4, 5, 5]
+    print(list(filter(lambda y: y % 2 == 0, x)))
+
+
+filter_lamb()
--- py-sets.py
+++ py-sets.py
@@ -1,6 +1,11 @@
 set1 = {1, 2, 3, 4, 5}
 set2 = {4, 5, 6, 7, 8}
-set3 =set1.union(set2)
+set3 = set1.union(set2)
 set4 = set1.intersection(set2)
 set5 = set1.difference(set2)
-print(set5)
+set6 = set1.symmetric_difference(set2)  # reverse of intersection
+set1.difference_update(set2)
+print(set1)
+
+# in-place method (it modifies the original set).
+# non-in-place method (it returns a new set).
